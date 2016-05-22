1a37242cb7f0bf457678941316f218af132c9f7a
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-05-22 11:31:23 -0400

    complete object quiz

--- object.js
+++ object.js
@@ -6,14 +6,6 @@ var person = {
   skills : ['JavaScript', 'HTML', 'CSS']
 };
 
-function print(message) {
-  var div = document.getElementById('output');
-  div.innerHTML = message;
+for (var key in person) {
+	console.log(key, ': ', person[key]);
 }
-
-var message = '<p>Hello. My name is ' + person.name + '</p>';
-message += '<p>I am from ' + person.country + '</p>';
-person.country = 'Brazil';
-message += '<p>But I wish I was from ' + person.country + '</p>';
-message += '<p>I have ' + person.skills.length + ' total skills: ' + person.skills.join(', ') + '</p>';
-print(message);
